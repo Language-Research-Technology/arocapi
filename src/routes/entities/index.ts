08c6d9b1742f29650597491a11c9dd6955a050a6
John Ferlito <johnf@inodes.org>
2025-05-15 11:36:00 +1000

    feat: start on entities

--- src/routes/entities/index.ts
+++ src/routes/entities/index.ts
@@ -1,7 +1,10 @@
 import type { FastifyPluginAsync } from 'fastify';
 
 const entities: FastifyPluginAsync = async (fastify, opts) => {
-  fastify.get('/', async (request, reply) => {});
+  fastify.get('/', async (request, reply) => ({
+    total: 0,
+    entities: [],
+  }));
 };
 
 export default entities;
