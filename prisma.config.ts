0681330bc51508dccbb4fb2258e2dc17b411a641
John Ferlito <johnf@inodes.org>
2025-08-27 08:35:29 +1000

    fix: finalise move to prisma multi-model

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,12 +1,6 @@
-import path from 'node:path';
-
 import 'dotenv/config';
 import { defineConfig } from 'prisma/config';
 
 export default defineConfig({
-  schema: path.join('prisma', 'schema.prisma'),
-  migrations: {
-    path: path.join('prisma', 'migrations'),
-    // seed: "prisma/seed.ts"
-  },
+  schema: 'prisma',
 });
