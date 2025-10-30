bfce95a84df6776195306b3d7e70987da9d710ee
John Ferlito <johnf@inodes.org>
2025-10-31 10:46:13 +1100

    fix: make changes for upcoming prisma v7

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,6 +1,11 @@
 import 'dotenv/config';
-import { defineConfig } from '@prisma/config';
+import { defineConfig, env } from '@prisma/config';
 
 export default defineConfig({
   schema: 'prisma',
+  engine: 'classic',
+  datasource: {
+    // provider: "mysql",
+    url: env('DATABASE_URL'),
+  },
 });
