c795522b8a409f51b4475f1cce32393afb217e4a
John Ferlito <johnf@inodes.org>
2026-03-16 09:56:27 +1100

    chore: fix tests that were not working on blank image

--- scripts/setup-integration.sh
+++ scripts/setup-integration.sh
@@ -6,4 +6,4 @@ docker compose -f docker-compose.test.yml up --wait --detach
 
 export DATABASE_URL='mysql://root:password@localhost:3307/catalog_test'
 prisma migrate reset --force
-npx prisma migrate dev --name init
+npx prisma migrate deploy
